216d947a99af8f31918e5ef5fd0de7252ed7fb57
Barbara Rodrigues <104439153+BarbaraSRodrigues@users.noreply.github.com>
2023-10-27 15:42:19 -0300

    Add files via upload

---
+++ maze/labirinto.py
@@ -0,0 +1,17 @@
+import networkx as nx
+from collections import deque
+
+numMaze = 0
+
+while True:
+
+    w = int(input())
+    h = int(input())
+
+    if ((w == 0) and (h == 0)):
+        break
+
+    
+
+    numMaze = numMaze + 1
+
