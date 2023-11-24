7a397fa93f11efb92f9eec91b60f26c39aeab2d3
Barbara Rodrigues <104439153+BarbaraSRodrigues@users.noreply.github.com>
2023-11-24 15:23:54 -0300

    Add files via upload

---
+++ bee/abelha.cpp
@@ -0,0 +1,14 @@
+/* Nome: Barbara Silveira Rodrigues
+   Materia: Desafios de Programação
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int willi, x, y;
+
+    while(cin >> willi){
+        
+    }
+}
