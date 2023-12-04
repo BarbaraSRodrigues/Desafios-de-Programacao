faff8b56a1d7eac144689b103c09e0083e2154ba
Barbara Rodrigues <104439153+BarbaraSRodrigues@users.noreply.github.com>
2023-12-04 17:27:22 -0300

    Add files via upload

---
+++ lcd.cpp
@@ -0,0 +1,31 @@
+/* 
+    Nome: Barbara Silveira Rodrigues
+    Materia: Desafios de Programação
+*/
+
+#include <iostream>
+#include <tuple>
+// #define A "-"
+// #define B "|"
+// #define C "|"
+// #define D "-"
+// #define E "|"
+// #define F "|"
+// #define G "-"
+using namespace std;
+
+int main(){
+
+    int s, n;  
+
+    
+    while(true){
+
+        cin >> s >> n;
+
+        if((s == 0) && (n == 0))
+            break;
+        
+        
+    }
+}
